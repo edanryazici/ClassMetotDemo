1c9661c91c256f9d8b4dec2684ad4c1612b0c56e
edanryazici <75092705+edanryazici@users.noreply.github.com>
2024-02-16 11:19:38 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(MusteriClass musteri)
+        {
+            Console.WriteLine("Musteri Eklendi! : " + musteri.Ad);
+        }
+
+        public void Sil(int id)
+        {
+            Console.WriteLine("Müşteri Silindi! Silinen müşteri no: " + id);
+        }
+
+        public void Listele(MusteriClass musteri)
+        {
+            Console.WriteLine("------ Müşteri Bilgileri -------");
+            Console.WriteLine(musteri.Id);
+            Console.WriteLine(musteri.Ad);
+            Console.WriteLine(musteri.Soyad);
+            Console.WriteLine(musteri.HesapNo);
+            Console.WriteLine("--------------");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MusteriClass musteri = new MusteriClass();
+            musteri.Id =1;
+            musteri.Ad = "Edanur";
+            musteri.Soyad = "Yazıcı";
+            musteri.HesapNo = "123456789";
+
+            MusteriClass musteri2 = new MusteriClass();
+            musteri2.Id = 2;
+            musteri2.Ad = "Bahar";
+            musteri2.Soyad = "Felek";
+            musteri2.HesapNo = "234567891";
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri);
+            musteriManager.Listele(musteri);
+            musteriManager.Listele(musteri2);
+            musteriManager.Sil(musteri2.Id);
+
+        }
+    }
+}
